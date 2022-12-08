9387259e1a95a7dee5bb36317e93780f0dd7d51d
tm1823 <119402015+tm1823@users.noreply.github.com>
2022-12-08 11:13:57 +0900

    Update indexfizzbuzz.php

--- 3-1/indexfizzbuzz.php
+++ 3-1/indexfizzbuzz.php
@@ -3,7 +3,9 @@ $num = 1;
 while($num < 101){
   
  
-  if($num % 3 == 0){
+  if($num % 3 == 0 && $num % 5 == 0){
+    echo "FizzBuzz!!";
+  } elseif($num % 3 == 0){
     echo "Fizz!";
   } elseif ($num % 5 == 0) {
     echo "Buzz!";
